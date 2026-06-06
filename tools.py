4bae88eff59b34f5a43a4f463f40edcb2d7322b3
CR4ZYM4D <madhavdixit482@gmail.com>
2026-06-07 02:38:35 +0530

    web search tool

---
+++ tools.py
@@ -0,0 +1,32 @@
+from langchain.tools import tool
+import requests
+
+from bs4 import BeautifulSoup
+
+from tavily import TavilyClient
+
+import os
+import sys
+from dotenv import load_dotenv
+load_dotenv()
+
+
+tavily = TavilyClient(api_key = os.getenv("TAVILY_API_KEY"))
+
+
+@tool
+def web_search(query: str, depth: str = 'advanced'):
+
+    try: 
+    
+        response = tavily.search(query = query, max_results = 5, search_depth=depth)
+        out = []
+        for r in response['results']:
+
+            out.append(f"title: {r['title']}\n url: {r['url']}\n snippet: {r['content'][:400]}")
+    
+        res = "\n------\n".join(out)
+        return res
+
+    except Exception as e:
+        raise(e, sys)
